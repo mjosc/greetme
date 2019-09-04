cd9b5a47ca1129a850ab4b236bcf82f09dcc042b
Matt Josse <matt.josse@drip.com>
2019-09-04 12:49:58 -0600

    add simple mock db and implement mock handler

--- pkg/mocks/mockapi/handlers/user_by_name.go
+++ pkg/mocks/mockapi/handlers/user_by_name.go
@@ -1,9 +1,21 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/mjosc/greetme/pkg/db/entities"
 	"github.com/mjosc/greetme/pkg/mocks/mockapi/mockops/users"
 )
+
+const UserNotFound = "UserNotFound"
+
+var database = map[string]*entities.User{
+	"Matt": &entities.User{
+		ID:   1,
+		Name: "Matt",
+	},
+}
 
 func NewUserByName() users.UserByNameHandler {
 	return &UserByName{}
@@ -12,9 +24,20 @@ func NewUserByName() users.UserByNameHandler {
 type UserByName struct {
 }
 
-func (h UserByName) Handle(users.UserByNameParams) middleware.Responder {
+func (h UserByName) Handle(params users.UserByNameParams) middleware.Responder {
+	var responder = users.NewUserByNameOK()
+
+	user, ok := database[params.Name]
+	if !ok {
+		return responder.WithPayload(&users.UserByNameOKBody{
+			Valid:   false,
+			Error:   UserNotFound,
+			Message: fmt.Sprintf("A user with the name %v does not exist", params.Name),
+		})
+	}
 	return users.NewUserByNameOK().WithPayload(&users.UserByNameOKBody{
-		ID:   1,
-		Name: "matt",
+		Valid: true,
+		ID:    user.ID,
+		Name:  user.Name,
 	})
 }
